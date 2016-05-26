0009818ef2481ee8c5898dcae9e4ea9c813656cc
xorond <xorond@archstrike.org>
2016-05-26 23:19:16 +0300

    moved working mosquito-git to main removed useless run.sh and added proper wrapper script into PKGBUILD. bumped pkgrel

--- archstrike-testing/mosquito-git/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# start whole mosquito toolkit on Arch based systems
-cd /usr/share/mosquito
-python2 mosquito/start.py 8082 4444 --http 8000
-echo "Please open http://localhost:8000/generate.html in your browser"
